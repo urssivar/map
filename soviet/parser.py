dcc0711481ada6082f61e6b04a9dc37619fbf4bc
alkaitagi <alkaitagi@outlook.com>
2023-11-01 15:49:14 +0300

    Add blocks

--- soviet/parser.py
+++ soviet/parser.py
@@ -1,3 +1,23 @@
-with open('book.txt', 'r', encoding="utf8") as f:
-    items = f.read().split('\n\n')
-    print(*items[:15], sep='\n\n')
+def block():
+    output = ""
+    with open('book.txt', 'r', encoding="utf8") as f:
+        lines = f.readlines()
+        for l in lines:
+            l = l.strip()
+            if l.isnumeric() or not l:
+                continue
+            if 'г.р' in l:
+                output += '\n\n'
+            output += ' ' + l
+
+    with open('blocks.txt', 'w', encoding="utf8") as f:
+        f.write(output)
+
+
+def split():
+    with open('blocks.txt', 'r', encoding="utf8") as f:
+        blocks = f.read().split('\n\n')
+        print(blocks[1])
+
+
+split()
