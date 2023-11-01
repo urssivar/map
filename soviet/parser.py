1807207aaa66b6ea5040c5e2af00f50876b5f285
alkaitagi <alkaitagi@outlook.com>
2023-11-01 16:43:32 +0300

    Prepare blocks 2

--- soviet/parser.py
+++ soviet/parser.py
@@ -1,14 +1,18 @@
+# book : г\.р -> \n$& // 8653
+
 def block():
     output = ""
     with open('book.txt', 'r', encoding="utf8") as f:
         lines = f.readlines()
+        block = ''
         for l in lines:
             l = l.strip()
             if l.isnumeric() or not l:
                 continue
             if 'г.р' in l:
-                output += '\n\n'
-            output += ' ' + l
+                output += block.strip() + '\n\n'
+                block = ''
+            block += l + ' '
 
     with open('blocks.txt', 'w', encoding="utf8") as f:
         f.write(output)
@@ -20,4 +24,4 @@ def split():
         print(blocks[1])
 
 
-split()
+block()
