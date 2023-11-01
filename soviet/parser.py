8a8f7cac3546100b18b1054524031dc346e35ef5
alkaitagi <alkaitagi@outlook.com>
2023-11-02 00:54:32 +0300

    Parse soviet repressions

--- soviet/parser.py
+++ soviet/parser.py
@@ -1,4 +1,6 @@
 # book : г\.р -> \n$& // 8653
+import re
+
 
 def block():
     output = ""
@@ -19,9 +21,51 @@ def block():
 
 
 def split():
-    with open('blocks.txt', 'r', encoding="utf8") as f:
-        blocks = f.read().split('\n\n')
-        print(blocks[1])
+    def sentence(x):
+        for r, s in [('концлагерь', 'лагер'),
+                     ('гулаг', 'ИТЛ'),
+                     ('расстрел', 'расстрел'),
+                     ('высылка', 'высылк'),
+                     ('ссылка', 'ссылк'),
+                     ('тюрьма', 'заключен'),
+                     ('тюрьма', 'свобод')]:
+            if s in x:
+                return r
+
+    with open('list.txt', 'w', encoding="utf8") as fw:
+        with open('blocks.txt', 'r', encoding="utf8") as f:
+            lines = f.readlines()
+            for l in lines:
+
+                try:
+                    name, b_year = re.findall(
+                        r'([а-яА-Я\s]*).*(\d{4}).*г\.р', l)[0]
+                    a_yr = re.findall(r'Арестован.*?(\d{4})', l)
+                    s_yr = re.findall(r'Приговорен.*?(\d{4})', l)
+                    r_yr = re.findall(r'Реабилитирован.*?(\d{4})', l)
+
+                    row = [name, b_year]
+                    for x in [a_yr, s_yr, r_yr]:
+                        if len(x) > 1:
+                            raise
+
+                    if a_yr:
+                        row.append(a_yr[0])
+                    elif s_yr:
+                        row.append(s_yr[0])
+                    else:
+                        row.append('ERR')
+                    row.append(r_yr[0])
+
+                    sent = sentence(l.split('заменен')[1]
+                                    if 'заменен' in l else l)
+                    row.append(sent if sent else 'ERR')
+
+                except:
+                    row = ['ERR']+['']*4
+
+                row.append(l)
+                fw.write('|'.join(row))
 
 
-block()
+split()
