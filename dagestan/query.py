5a182df00789c07322b881a2d60973c526aca35f
alkaitagi <alkaitagi@outlook.com>
2023-11-04 23:17:27 +0300

    Write scrapper for familio

--- dagestan/query.py
+++ dagestan/query.py
@@ -1,3 +1,19 @@
-url = 'https://familio.org/api/v1/catalogs/dagestan1888/records?parent=7906ddd7-8a43-4c48-a0db-ad2a8ec0e560&page=1'
+from urllib.request import urlopen
+import json
+import re
 
-# parent = наибство
+naib_id = 'fd1db04b-9c70-4b40-84a1-70091452f421'
+
+vills = []
+for i in range(1, 100):
+    u = f'https://familio.org/api/v1/catalogs/dagestan1888/records?parent={naib_id}&page={i}'
+    text = json.dumps(
+        json.loads(urlopen(u).read()),
+        ensure_ascii=False)
+    vs = re.findall(r'"record_text": "(.*?)"', text)
+    if not vs:
+        break
+    vills += vs
+
+with open('list.txt', 'w', encoding="utf8") as f:
+    f.write('\n'.join(vills[:-1]))
